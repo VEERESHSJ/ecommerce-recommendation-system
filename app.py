fe1db06c23858ce50a5268919cb14fa8ddc7513d
VEERESH <virajsj2@gmail.com>
2026-04-30 17:56:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+import streamlit as st
+import pandas as pd
 
+st.title("E-Commerce Recommendation System")
+
+# simple working data
+data = {
+    "User": [1, 1, 2, 2],
+    "Product": ["Mobile", "Laptop", "Laptop", "Headphones"],
+    "Rating": [5, 4, 5, 3]
+}
+
+df = pd.DataFrame(data)
+
+st.write("App is working ✅")
+st.write(df)
